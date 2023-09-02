3e21bb4161c6148fba4d6af346d61025b753ba26
Alessia Angeli <114294704+alessia-angeli@users.noreply.github.com>
2023-09-02 17:39:48 +0200

    Add files via upload

--- controllers/gestore.js
+++ controllers/gestore.js
@@ -15,20 +15,6 @@ const get_gestore = (req, res) => {
     });
 };
 
-
-// GET '/gestore'
-// const show_gestori = (req, res) => {
-
-//     //find the specific ingrediente with that name
-//     Gestori.find({ }, (err, data) => {
-//         if (err || !data) {
-//             return res.status(400).json({ message: "Ingredienti not present." });
-//         }
-//         else return res.status(200).json(data); //return the ingredienti objects if found
-//     });
-// }
-
 module.exports = { 
-    get_gestore,
-    // show_gestori
+    get_gestore
 }
